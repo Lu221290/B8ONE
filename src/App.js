fd29ebef1161fede477ce98d12166cfd5ad69a45
Lucas Marques <79729302+Lu221290@users.noreply.github.com>
2021-11-11 15:27:42 -0300

    B8ONE

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, } from 'react';
+import Imagem1 from './imagem/imagem1.png';
+import './style.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            botao:false,
+            feed:[
+                {id: 1, modelo: 'Monitor LED 27" Gamer Curvo', marca: 'Sansung 1920 x 1080 FHD 240 Hz', entrada:'HDMI, DP, Gaync Série CRG50', valor:2830, promocao:2599, parcelado:259},
+                {id: 2, modelo: 'Monitor LED 27" Gamer Curvo', marca: 'Sansung 1920 x 1080 FHD 240 Hz', entrada:'HDMI, DP, Gaync Série CRG50',valor:2830, promocao:2599, parcelado:259},
+            ]
+
+           
+        }
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick(){
+        this.setState(state => ({
+            botao: !state.botao
+        }));
+    }
+
+    render(){
+        return (
+            <div className="App">
+                {this.state.feed.map((item) =>{
+                    return(
+                        <div key={item.id}>
+                            <img src={Imagem1} />
+                            <p>{item.modelo}</p>
+                            <p>{item.marca}</p>
+                            <p>{item.entrada}</p>
+                            <h4>R${item.valor},00</h4>
+                            <h2>R${item.promocao},00</h2>
+                            <h5>em até 10X de R${item.parcelado},90</h5>
+                            <button onClick={this.handleClick}>
+                    {this.state.botao ? "ADICIONAR" : "ADICIONADO"}
+                </button>
+                        </div>
+                    )
+                })}
+                
+            </div>
+        )
+    }
 }
 
 export default App;
